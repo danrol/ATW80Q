7ed0c746cd6c10746527a052e3f4ab483fed6440
elia_ba <elia.ben.anat@s.afeka.ac.il>
2018-11-05 12:47:47 +0200

    test

--- src/playground/Application.java
+++ src/playground/Application.java
@@ -2,8 +2,10 @@ package playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:resources/application.properties")
 public class Application {
 	
 	public static void main(String[] args) {
