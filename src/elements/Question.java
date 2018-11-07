5290677a3c9ed48f04302ec08a98cb972063e1eb
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2018-11-07 11:17:30 +0200

    Session + Question

--- src/elements/Question.java
+++ src/elements/Question.java
@@ -3,27 +3,29 @@ package elements;
 import java.util.ArrayList;
 
 public class Question {
-	private int id;
 	private String questionBody;
-	//private ArrayList<String> answers;
+	private ArrayList<String> answers;
 	private String correctAnswer;
-	public Question(int id, String questionBody, String correctAnswer) {
+
+	public Question(String questionBody, ArrayList<String> answers, String correctAnswer) {
 		super();
-		this.id = id;
 		this.questionBody = questionBody;
+		this.answers = answers;
 		this.correctAnswer = correctAnswer;
+		
 	}
-	
+
 	public void setQuestionBody(String questionBody) {
 		this.questionBody = questionBody;
 	}
-	
+
 	public void setCorrectAnswer(String correctAnswer) {
 		this.correctAnswer = correctAnswer;
 	}
-	
+
 	public void editQuestion(String questionBody, String correctAnswer) {
 		this.questionBody = questionBody;
 		this.correctAnswer = correctAnswer;
 	}
+	// return array of answers
 }
