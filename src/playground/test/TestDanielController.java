5f843e1fe21fc98a335fe41c9669b4923e831a2b
Daniel Rolnik <danrol92@gmail.com>
2018-11-16 22:53:13 +0200

    added class TestDanielController.java

---
+++ src/playground/test/TestDanielController.java
@@ -0,0 +1,46 @@
+package playground.test;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class TestDanielController {
+	
+	private RestTemplate restTemplate;
+	
+	@LocalServerPort
+	private int port;
+	private String url;
+
+	@PostConstruct
+	public void init() {
+		this.restTemplate = new RestTemplate();
+		this.url = "http://localhost:" + port + "/messages";
+		System.err.println(this.url);
+	}
+	
+	@Before
+	public void setup() {
+		
+	}
+
+	@After
+	public void teardown() {
+	}
+	
+	@Test
+	public void testRegisterNewUser() {
+		
+	}
+	
+}
