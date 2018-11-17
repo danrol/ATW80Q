23bfdb138871260c119ecd2a5d91159624f2662b
Eden Dupont <eg7eg7@gmail.com>
2018-11-17 14:49:34 +0200

    UnitTest classes

---
+++ src/playground/test/TestEliaController.java
@@ -0,0 +1,51 @@
+package playground.test;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import playground.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class TestEliaController {
+	
+	private RestTemplate restTemplate;
+	
+	@LocalServerPort
+	private int port;
+	private String url;
+
+	@PostConstruct
+	public void init() {
+		this.restTemplate = new RestTemplate();
+		this.url = "http://localhost:" + port + "/messages";
+		System.err.println(this.url);
+	}
+	
+	@Before
+	public void setup() {
+		
+	}
+
+	@After
+	public void teardown() {
+	}
+
+	@Test
+	public void testServerIsBootingCorrectly() throws Exception {
+		
+	}
+	
+	@Test
+	public void testRegisterNewUser() {
+		
+	}
+	
+}
