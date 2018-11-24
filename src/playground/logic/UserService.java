8b50e4a50bca6fcf2fed7a3766c19f78bb24bac2
Eden Dupont <eg7eg7@gmail.com>
2018-11-24 21:49:40 +0200

    UserService Dummy Data

--- src/playground/logic/UserService.java
+++ src/playground/logic/UserService.java
@@ -17,15 +17,18 @@ public class UserService implements Serializable{
 	private static ArrayList<UserEntity> users = new ArrayList<UserEntity>();
 
 	public UserService() {
+		addDummyDatabase();
+	}
+
+	public void addDummyDatabase()
+	{
 		UserEntity user1 = new UserEntity("username1", "username1@gmail.com", "avatar1", "moderator", Constants.PLAYGROUND_NAME, "");
 		UserEntity user2 = new UserEntity("username2", "username2@gmail.com", "avatar2", "player", Constants.PLAYGROUND_NAME, "abc");
-		user2.setPlayground(Constants.PLAYGROUND_NAME);
 		UserEntity user3 = new UserEntity("username2", "username3@gmail.com", "avatar3", "player", Constants.PLAYGROUND_NAME, "blabla");
 		this.addUser(user1);
 		this.addUser(user2);
 		this.addUser(user3);
 	}
-
 
 	public static ArrayList<UserEntity> getUsers() {
 		return users;
