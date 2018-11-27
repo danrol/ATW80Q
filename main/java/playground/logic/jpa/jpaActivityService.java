e2ff75d26bed3e66fa6c0b8a1efbb7552734b6dd
elia_ba <elia.ben.anat@s.afeka.ac.il>
2018-11-27 12:15:40 +0200

    services build

---
+++ main/java/playground/logic/jpa/jpaActivityService.java
@@ -0,0 +1,24 @@
+package playground.logic.jpa;
+
+import org.springframework.stereotype.Service;
+
+import playground.logic.ActivityService;
+import playground.logic.ElementService;
+import playground.logic.UserService;
+
+@Service
+public class jpaActivityService implements ActivityService {
+
+	@Override
+	public void setElementService(ElementService elementService) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUserService(UserService userService) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
