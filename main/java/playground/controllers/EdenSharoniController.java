8c2b22194208a097fe499fd8eda8964f4d46a3ef
Eden_Sharoni1995 <eden.sharoni@s.afeka.ac.il>
2018-12-02 09:32:43 +0200

    Revert "Update EdenSharoniController.java"
    This reverts commit f8b7662da5b31124e30a46cb77955e2644ed3b7c.

--- main/java/playground/controllers/EdenSharoniController.java
+++ main/java/playground/controllers/EdenSharoniController.java
@@ -11,6 +11,7 @@ import playground.Constants;
 import playground.exceptions.ChangeUserException;
 import playground.exceptions.LoginException;
 import playground.layout.UserTO;
+import playground.logic.ElementService;
 import playground.logic.UserEntity;
 import playground.logic.UserService;
 
@@ -18,8 +19,14 @@ import playground.logic.UserService;
 @RestController
 public class EdenSharoniController {
 
+	private ElementService elementService;
 	private UserService userService;
 	
+	
+	@Autowired
+	public void setElementService(ElementService elementService){
+		this.elementService = elementService;
+	}
 	
 	@Autowired
 	public void setUserService(UserService userService){
