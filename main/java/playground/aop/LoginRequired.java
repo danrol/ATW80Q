5e632577744f0feb8847dc4dee01826ae00256df
Eden Dupont <eg7eg7@gmail.com>
2018-12-18 17:17:37 +0200

    Changed Authentication to Login

--- main/java/playground/aop/LoginRequired.java
+++ main/java/playground/aop/LoginRequired.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
 @Target(PARAMETER)
-public @interface MyAuthentication {
+public @interface LoginRequired {
 
 }
---
+++ main/java/playground/aop/LoginRequiredAspect.java
@@ -0,0 +1,26 @@
+package playground.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoginRequiredAspect {
+	
+
+	@Before("@annotation(playground.aop.LoginRequired) && args(userPlayground,email,..)")
+	public Object Login(ProceedingJoinPoint joinPoint, String userPlayground, String email) throws Throwable {
+		//TODO
+		
+		System.err.println("Login here stub");
+		if (userPlayground == null || email.length() < 5) {
+			throw new RuntimeException("Invalid name: " + email + " - it is too short");
+		}
+		return joinPoint.proceed();
+	}
+	 
+
+}
