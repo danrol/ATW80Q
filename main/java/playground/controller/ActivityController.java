aa0c2814cefa6f2f02254c1c111a6fe56c016c96
Eden Dupont <eg7eg7@gmail.com>
2018-12-22 14:47:05 +0200

    Update ActivityController.java

--- main/java/playground/controller/ActivityController.java
+++ main/java/playground/controller/ActivityController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import playground.aop.LoginRequired;
 import playground.layout.ActivityTO;
+import playground.logic.ActivityEntity;
+import playground.logic.ActivityService;
 import playground.logic.ElementService;
 import playground.logic.UserService;
 
@@ -19,7 +22,7 @@ public class ActivityController {
 
 	private ElementService elementService;
 	private UserService userService;
-
+	private ActivityService activityService;
 
 	@Autowired
 	public void setElementService(ElementService elementService){
@@ -31,15 +34,21 @@ public class ActivityController {
 		this.userService = userService;
 	}
 	
+	@Autowired
+	public void setActivityService(ActivityService activityService){
+		this.activityService = activityService;
+	}
 	
+	@LoginRequired
 	@RequestMapping(method=RequestMethod.POST,path="/playground/activities/{userPlayground}/{email}",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
-	public Object RequestServer(@RequestBody ActivityTO activity, @PathVariable("email") String email,@PathVariable("userPlayground") String userPlayground) 
+	public Object RequestServer(@PathVariable("userPlayground") String userPlayground, @PathVariable("email") String email,@RequestBody ActivityTO activity) 
 	{
 		/* function 11
 		 * INPUT: ActivityTO
 		 * OUTPUT: Object
 		 */
-		return activity; 
+		ActivityEntity t = activityService.addActivity(activity.toEntity());
+		return new ActivityTO(t); 
 	}
 	
 	
