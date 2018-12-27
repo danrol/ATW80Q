fccc45325a859f3b77cc9f420be33314a186c170
Eden Dupont <eg7eg7@gmail.com>
2018-12-28 00:31:19 +0200

    LoginRequired FIX

--- main/java/playground/aop/LoginRequiredAspect.java
+++ main/java/playground/aop/LoginRequiredAspect.java
@@ -1,13 +1,12 @@
 package playground.aop;
-import java.util.Arrays;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import playground.dal.UserDao;
-import playground.logic.UserService;
+import playground.exceptions.LoginException;
+import playground.logic.UserEntity;
 
 
 @Component
@@ -22,16 +21,15 @@ private UserDao userDB;
 public LoginRequiredAspect(UserDao userDB) {
 	this.userDB = userDB;
 }
-	
-	@Around("@annotation(playground.aop.LoginRequired)")
-	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {//String userPlayground, String email, String requiredRole
-		System.err.println("IN LoginRequired");
-//		UserEntity u = userDB.findById(UserEntity.createKey(email, userPlayground)).orElse(null);
-//		if (u == null) 
-//			throw new LoginException("Email is not registered.");
-//			else if(!u.isVerified()) 
-//				throw new LoginException("User is not verified.");
-//				
+	@MyLog
+	@Around("@annotation(playground.aop.LoginRequired) && args(userPlayground, email,..)")
+	public Object log(ProceedingJoinPoint joinPoint, String userPlayground, String email) throws Throwable {
+		UserEntity u = userDB.findById(UserEntity.createKey(email, userPlayground)).orElse(null);
+		if (u == null) 
+			throw new LoginException("Email is not registered.");
+			else if(!u.isVerified()) 
+				throw new LoginException("User is not verified.");
+				
 		Object o = joinPoint.proceed(joinPoint.getArgs());
 		return o;
 	}
