6e37d86fd7fd621fd3a40ea82f2f092d35355d2e
Daniel Rolnik <danrol92@gmail.com>
2018-12-30 10:24:49 +0200

    distanceIsGreaterThanZero fixed for dummy element

--- main/java/playground/logic/stubs/DummyElementService.java
+++ main/java/playground/logic/stubs/DummyElementService.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
-@Service
+//@Service
 public class DummyElementService implements ElementService {
 
 	/**
